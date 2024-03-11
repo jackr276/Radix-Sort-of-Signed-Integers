c77f954e86e46f314aec1fd24a179e94d4e91773
jackr276 <jack_m_robbins@me.com>
2024-03-11 10:21:33 -0400

    Update radix_sort.c

--- src/radix_sort.c
+++ src/radix_sort.c
@@ -1,6 +1,9 @@
 /**
  * Author: Jack Robbins
- * Homework 04, only problem(radix sort of signed integers)
+ * This C program implements a hexadecimal radix sort of signed integers, using a hexadecimal
+ * radix sort algorithm. Memory allocation is done manually using malloc. The program has been
+ * tested and is memory safe. Radix Sort results in an O(n) running time, making it the most efficient
+ * known sorting algorithm when dealing with data that can be sorted using its binary format
  */
 
 #include <stdlib.h>
@@ -9,12 +12,11 @@
 
 /**
  * The main function simply makes calls to the radix_sort_unsigned and
- * to_signed_order functions, after it gets input from bash
+ * to_signed_order functions, after it gets input from stdin
  */
 int main(void){
-	//Predeclare the radix_sort_unsigned function
+	//Predeclare radix_sort_unsigned and to_signed_order functions
 	void radix_sort_unsigned(unsigned int* arr, int size);
-	//Predeclare to_signed_order
 	void to_signed_order(unsigned int* arr, int size);
 
 	//We assume the first thing entered is the number of integers to be sorted
@@ -55,7 +57,7 @@ int main(void){
  * Using hexadecimal radix sort -- 16 buckets, 4 bits chunks each time
  */
 void radix_sort_unsigned(unsigned int* arr, int size){
-	//predeclare the swap and resetArr functions
+	//Predeclare swap and resetArr functions
 	void swap(unsigned int** arr1, unsigned int** arr2);
 	void resetArr(int* arr, int size);
 	
@@ -68,7 +70,7 @@ void radix_sort_unsigned(unsigned int* arr, int size){
 	unsigned int mask = 0xF;
 
 	//declare counters and offset arrays
-	//We need an array of counters every time this for loop runs
+	//We need an array of counters every(this will be reset to 0 every iteration)
 	int* counters = (int*)malloc(16*sizeof(int));
 	//We also need an array of offsets, to represent bucket locations
 	int* offsets = ( int*)malloc(16*sizeof(int));
@@ -102,7 +104,6 @@ void radix_sort_unsigned(unsigned int* arr, int size){
 			buffer[offsets[chunk]] = arr[j];
 			//Increment offsets at that chunk to move the start of this bucket up by 1
 			offsets[chunk]++;
-
 		}
 
 		//We now swap array and buffer, as buffer is now the most properly sorted one
@@ -125,7 +126,6 @@ void swap(unsigned int** arr1, unsigned int** arr2){
 	unsigned int* temp;
 	//store arr1 in temp, set arr1 to arr2, then set arr2 to temp(old arr1)
 	temp = *arr1;
-	//set the array 1 pointer to the array 2 pointer
 	*arr1 = *arr2;
 	*arr2 = temp;
 }
